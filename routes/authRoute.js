c02773a355c8d1521d7b696dfb4ac6d28d05a15b
mr-aman49 <amanraj.87892@gmail.com>
2023-06-21 23:12:07 +0530

    first commit

---
+++ routes/authRoute.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import {
+    registerController, 
+    loginController,
+    testController,
+} from '../controllers/authController.js';
+
+// router object
+
+const router =express.Router();
+
+//routing
+//Register || METHOD POST
+router.post('/register', registerController) ;
+
+//LOGIN || POST
+router.post('/login',loginController);
+
+//test routes
+router.get('/test',testController);
+export default router;
